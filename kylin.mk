c2ffa2b74ca642359b8c7fbae87aad090b588474
zerojim kylin <kylinmod@gmail.com>
2014-03-05 20:32:00 +0800

    Kylin Modify

---
+++ kylin.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Inherit some common KM stuff.
+$(call inherit-product, vendor/kylin/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/kylin/config/nfc_enhanced.mk)
+
+# Inherit some common KM stuff.
+$(call inherit-product, vendor/kylin/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := km_hlte
